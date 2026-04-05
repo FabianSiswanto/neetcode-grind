75349922a3c316aa2836f0102b09e0c46ce2e2ed
Fabian Siswanto <136670600+FabianSiswanto@users.noreply.github.com>
2026-04-05 12:34:19 -0700

    Add: remove-all-adjacent-duplicates-in-string-ii - submission-0

---
+++ Data Structures & Algorithms/remove-all-adjacent-duplicates-in-string-ii/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        '''
+        brute force -> restart everytime remove stuff
+        -> optimize by removal from middle -> continue from there
+        -> must be able to remove from middle -> pop -> use stack
+
+        STORE STATE: [char, count]
+
+        check:
+        - if same item -> increment count of top
+        - if diff item -> append to stack, with count of 1
+        - after increment count -> if top item count == k -> pop
+        '''
+        stack = []
+
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+            else:
+                stack.append([c, 1])
+            
+            if stack and stack[-1][1] == k:
+                stack.pop()
+
+        res = ""
+        for char, count in stack:
+            res += char * count
+
+        return res
+                
+                
+
+
+        
